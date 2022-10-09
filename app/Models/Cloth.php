21b5bf983d4932eb5fe6b6d691b6a83adc7af9c7
masaya <masaya.reon@gmail.com>
2022-10-09 17:30:22 +0900

    ユーザーの服の情報取得モデル

--- app/Models/Cloth.php
+++ app/Models/Cloth.php
@@ -4,9 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Auth;
 class Cloth extends Model
 {
     use HasFactory;
+    protected $table="clothes";
+    protected $fillable=[
+        "sleeve",
+        "color",
+        "image"
+    ];
+    public static function get_user_clothes(){
+        $user_id=Auth::user();
+        return self::where("user_id",$user_id)->get();
+    }
     
 }
