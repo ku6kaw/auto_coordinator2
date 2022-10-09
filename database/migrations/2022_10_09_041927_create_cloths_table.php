b0452878112c703d88e9ac168d14727b27f24ca8
ku6kaw <reoreo01.123@gmail.com>
2022-10-09 17:16:54 +0900

    rename clothes_table

--- database/migrations/2022_10_09_041927_create_cloths_table.php
+++ database/migrations/2022_10_09_041927_create_cloths_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('cloths', function (Blueprint $table) {
+        Schema::create('clothes', function (Blueprint $table) {
             $table->id();
             $table->integer("user_id");
             $table->integer("sleeve");
