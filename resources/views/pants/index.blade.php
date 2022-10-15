d6b69a16063bc5fc6af042cfea131a4b77964e52
ku6kaw <reoreo01.123@gmail.com>
2022-10-15 13:33:14 +0900

    style:indexの画像表示を整えました

--- resources/views/pants/index.blade.php
+++ resources/views/pants/index.blade.php
@@ -14,73 +14,33 @@
   </x-slot>
 
   <div class="py-12">
-    <div class="max-w-7xl mx-auto sm:w-10/12 md:w-8/10 lg:w-8/12">
+    <div class="max-w-7xl mx-auto sm:w-1/2 md:w-1/2 lg:w-4/5">
       <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 bg-white border-b border-gray-200">
-          <table class="text-center w-full border-collapse">
+          <table class="w-full border-collapse">
             <thead>
               <tr>
-                <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-lg text-grey-dark border-b border-grey-light">Pants</th>
+                <th class="w-1/2 border-b border-gray-200">Long Sleeve</th>
+                <th class="w-1/2 border-b border-gray-200">Short Sleeve</th>
               </tr>
             </thead>
             <tbody>
-               @foreach ($long_pants as $pant)
-              <tr class="hover:bg-grey-lighter">
-
-                <td class="py-4 px-6 border-b border-grey-light">
-                  @if($loop->first)
-                  <div class="text-center font-bold text-lg text-grey-dark">
-                    Long pants
-                  </div>
-                  @endif
-                   <a href="{{ route('pants.show',$pant->id) }}">
-                    <h3 class="text-left font-bold text-lg text-grey-dark"><img src=" {{asset('storage/'.$pant->image)}}"></h3>
+              <tr>
+                <td class="w-1/2 justify-items-center">
+                @foreach ($long_pants as $pant)
+                  <a href="{{ route('pants.show',$pant->id) }}">
+                    <img src=" {{asset('storage/'.$pant->image)}}" class="max-h-60 max-w-1/2 rounded-lg hover:shadow-xl">
                   </a> 
-                                                                         <!--ここに写真が来るようにする今は写真のパスが来るようにしてる-->
-                  <div class="flex">
-                    <!-- 更新ボタン -->
-                    <!-- 削除ボタン -->
-                    <form action="{{ route('pants.destroy',$pant->id) }}" method="POST" class="text-left">
-                      @method('delete')
-                      @csrf
-                      <button type="submit" class="mr-2 ml-2 text-sm hover:bg-gray-200 hover:shadow-none text-white py-1 px-2 focus:outline-none focus:shadow-outline">
-                        <svg class="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="black">
-                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                        </svg>
-                      </button>
-                    </form>
-                  </div>
+                @endforeach
+                </td>
+                <td class="w-1/2 justify-items-center">
+                @foreach ($short_pants as $pant)        
+                  <a href="{{ route('pants.show',$pant->id) }}">
+                    <img src=" {{asset('storage/'.$pant->image)}}" class="max-h-60 max-w-1/2 rounded-lg hover:shadow-xl">
+                  </a> 
+                @endforeach
                 </td>
               </tr>
-              @endforeach
-              @foreach ($short_pants as $pant)
-              <tr class="hover:bg-grey-lighter">
-                <td class="py-4 px-6 border-b border-grey-light">
-                  @if($loop->first)
-                  <div class="text-center font-bold text-lg text-grey-dark">
-                    Short pants
-                  </div>
-                  @endif
-                   <a href="{{ route('pants.show',$pant->id) }}">
-                    <h3 class="text-left font-bold text-lg text-grey-dark"><img src=" {{asset('storage/'.$pant->image)}}"></h3>
-                  </a> 
-                                                                         <!--ここに写真が来るようにする今は写真のパスが来るようにしてる-->
-                  <div class="flex">
-                    <!-- 更新ボタン -->
-                    <!-- 削除ボタン -->
-                    <form action="{{ route('pants.destroy',$pant->id) }}" method="POST" class="text-left">
-                      @method('delete')
-                      @csrf
-                      <button type="submit" class="mr-2 ml-2 text-sm hover:bg-gray-200 hover:shadow-none text-white py-1 px-2 focus:outline-none focus:shadow-outline">
-                        <svg class="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="black">
-                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                        </svg>
-                      </button>
-                    </form>
-                  </div>
-                </td>
-              </tr>
-              @endforeach
             </tbody>
           </table>
         </div>
