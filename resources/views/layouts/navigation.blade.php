97cbb1b540d389188f32a801f18d1da84078dc11
masaya <masaya.reon@gmail.com>
2022-10-16 17:01:43 +0900

    ナビゲーションバー修正

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -29,7 +29,13 @@
             {{ __('Index') }}
           </x-nav-link>
         </div>
+          <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+          <x-nav-link :href="route('favorites.index')" :active="request()->routeIs('favorites.index')">
+            {{ __('Favorite') }}
+          </x-nav-link>
+        </div>
       </div>
+      
       
       <!-- Settings Dropdown -->
       <div class="hidden sm:flex sm:items-center sm:ml-6">
