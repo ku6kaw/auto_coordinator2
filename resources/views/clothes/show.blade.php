54f1f1f6fae5a7911242896462f55c6dd7c3915b
ku6kaw <reoreo01.123@gmail.com>
2022-10-16 15:08:11 +0900

    style:ダッシュボードに名前を表示

--- resources/views/clothes/show.blade.php
+++ resources/views/clothes/show.blade.php
@@ -19,39 +19,40 @@
                 <img src=" {{asset('storage/'.$cloth->image)}}">
               </p>
             </div>
-            <!-- imageここまで -->
-
             
+            <!-- Timestamp -->
             <div class="flex flex-col mb-4">
               <p class="mb-2 uppercase font-bold text-lg text-grey-darkest">Created_at</p>
               <p class="py-2 px-3 text-grey-darkest" id="created_at">
                 {{$cloth->created_at}}
               </p>
             </div>
+
             <div class="flex">
-                <!-- 削除ボタン -->
-
-                <script>
-                function confirm_test() {
-                    var select = confirm("削除しますか？");
-                    return select;
-                }
-                </script>
-
-                <form action="{{ route('clothes.destroy',$cloth->id) }}" method="POST" onsubmit="return confirm_test()" class="text-left">
-                    @method('delete')
-                    @csrf
-                    <button type="submit" class="mr-2 ml-2 text-sm hover:bg-gray-200 hover:shadow-none text-white py-1 px-2 focus:outline-none focus:shadow-outline">
-                    <svg class="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="black">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                    </svg>
-                    </button>
-                </form>
+              <!-- 削除確認用 -->
+              <script>
+              function confirm_test() {
+                  var select = confirm("削除しますか？");
+                  return select;
+              }
+              </script>
+              
+              <!-- 削除ボタン -->
+              <form action="{{ route('clothes.destroy',$cloth->id) }}" method="POST" onsubmit="return confirm_test()" class="text-left">
+              @method('delete')
+              @csrf
+                <button type="submit" class="mr-2 ml-2 text-sm hover:bg-gray-200 hover:shadow-none text-white py-1 px-2 focus:outline-none focus:shadow-outline">
+                <svg class="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="black">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                </svg>
+                </button>
+              </form>
             </div>
 
             <a href="{{ route('clothes.index') }}" class="block text-center w-full py-3 mt-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none">
               Back
             </a>
+
           </div>
         </div>
       </div>
