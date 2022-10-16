7cbc1c784d1720649e35dfa061108c391368e989
ku6kaw <reoreo01.123@gmail.com>
2022-10-16 14:00:52 +0900

    style:ナビゲーションバーをいじりました

--- resources/views/clothes/create.blade.php
+++ resources/views/clothes/create.blade.php
@@ -24,11 +24,11 @@
               <label class="mb-2 uppercase font-bold text-lg text-grey-darkest" for="length">length</label>
             <div class="flex items-center mb-4">
                 <input id="long_sleeve" type="radio" value="0" name="sleeve" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                <label for="long_sleeve" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Long sleeve</label>
+                <label for="long_sleeve" class="ml-2 text-sm font-medium text-grey-darkest">Long sleeve</label>
             </div>
             <div class="flex items-center">
                 <input checked id="short_sleeve" type="radio" value="1" name="sleeve" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                <label for="short_sleeve" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Short sleeve</label>
+                <label for="short_sleeve" class="ml-2 text-sm font-medium text-grey-darkest">Short sleeve</label>
             </div>
             </div>
              <div class="flex flex-col mb-4">
